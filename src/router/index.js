af6ef9df0c0eec2e9b70864a51972712ad1e1fb2
qq1176989241 <1176989241@qq.com>
2020-12-17 21:45:38 +0800

    fix: router hash

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import {data} from './data.json'
 import Home from '../views/Home.vue'
 
@@ -19,7 +19,7 @@ data.map((item) => {
 })
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHashHistory(),
   scrollBehavior () {
     return { top: 0 }
   },
