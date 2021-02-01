0217ce4bc413e8610228106e5138d9d16dbedd1a
yanghuanrong <1176989241@qq.com>
2021-02-01 20:06:08 +0800

    chore: build config

--- relax-plus/config/rollup.config.js
+++ relax-plus/config/rollup.config.js
@@ -1,12 +1,23 @@
+import pkg from '../../package.json'
 import vue from 'rollup-plugin-vue';
 import {uglify} from "rollup-plugin-uglify";
 import babel from 'rollup-plugin-babel';
 import path from 'path'
 import json from '@rollup/plugin-json'
 
+const createBanner = () => {
+  const time = new Date().toLocaleDateString()
+  return `/*!
+  * ${pkg.name} v${pkg.version}
+  * author ${pkg.author}
+  * updated date ${time}
+  */`
+}
+
 export default {
   input: path.resolve('relax-plus/package/index.js'),
   output: {
+      banner: createBanner(),
       format: 'umd',
       name: 'RelaxPlus',
       file: path.resolve('lib/relax.min.js')
